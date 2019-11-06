d3f4132dbedcf8c313d164cc627a198a4b3caf9d
Nicole Gumina <nicolegumina@nicoles-mbp.home>
2019-11-06 00:13:03 -0600

    created ranking for the cards and different alert messages

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,48 @@
 console.log("Up and running!")
-const cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank:"queen",
+	suit:"hearts",
+	cardImage: "images2/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images2/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images2/king-of-hearts"
 
-const cardsInPlay = [];
+},
 
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"images2/king-of-diamonds.png"
+}
+
+];
+
+var cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again.")
 	}
-}
+};
 
 function flipCard(cardId) {
+
 	console.log("User flipped " + cards[cardId.rank);
-	cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank); 
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	}
+	console.log(cards.cardImage);
+	console.log(cards.suit);
 
 }
 
