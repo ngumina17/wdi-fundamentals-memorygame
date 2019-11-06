a6fea16eb148ca4ccaf7f05a26a106d4e984b63b
Nicole Gumina <nicolegumina@nicoles-mbp.home>
2019-11-05 23:49:10 -0600

    created alert windows and more functionality

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,18 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cards[0]);
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cards[2]);
-console.log("User flipped king");
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry,try again");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.")
+	}
 }
 
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId.rank);
+	cardsInPlay.push(cards[cardId].rank);
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+}
+
+}
