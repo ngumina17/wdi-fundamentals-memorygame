550bf5709bedb327476b149cb51db30704b2d71f
Nicole Gumina <nicolegumina@nicoles-mbp.home>
2019-11-05 22:13:04 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!")
+const cards = ["queen", "queen", "king", "king"];
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
