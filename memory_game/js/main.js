f84c85c5a10db52d026044edde1beaa77510efc3
Nicole Gumina <nicolegumina@nicoles-mbp.home>
2019-11-05 22:59:20 -0600

    created game logic, added functionality, and alerts

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,22 @@
 console.log("Up and running!")
 const cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry,try again");
+}
+
 
 console.log("User flipped " + cardTwo);
 console.log("User flipped " + cardFour);
